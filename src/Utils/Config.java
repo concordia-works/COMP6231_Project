1e7c88ba30207d4ac1bcb1636560c7243eb36152
Quoc Minh VU <minhvq27@gmail.com>
2017-07-20 19:53:17 -0400

    Update Config

--- src/Utils/Config.java
+++ src/Utils/Config.java
@@ -1,4 +1,24 @@
 package Utils;
 
 public class Config {
+    public static class ARCHITECTURE {
+        public enum REPLICAS {MINH, KAMAL, KEN_RO}
+    }
+
+    public static class REQUEST {
+        public enum METHODS_NAME {createTRecord, createSRecord, getRecordsCount,
+                                  editRecord, transferRecord,printRecord, printAllRecords}
+    }
+
+    public static class CORBA {
+        public static final String ORB_PARAMETERS = "-ORBInitialPort 1050 -ORBInitialHost localhost";
+        public static final String ROOT_POA = "RootPOA";
+        public static final String NAME_SERVICE = "NameService";
+    }
+
+    public static class UDP {
+        public static final int PORT_FRONT_END_TO_LEADER = 123;
+        public static final int PORT_LEADER_TO_BACKUPS = 234;
+        public static final int PORT_HEART_BEAT = 345;
+    }
 }
