5b2df99a3584293300f62cd04c1e4ce7930297ab
Quoc Minh VU <minhvq27@gmail.com>
2017-07-28 16:51:30 -0400

    FrontEnd now can send request to Leader and receive response

--- src/FrontEnd/FrontEndImpl.java
+++ src/FrontEnd/FrontEndImpl.java
@@ -1,4 +1,77 @@
 package FrontEnd;
 
-public class FrontEndImpl {
+import Servers.FIFO;
+import Utils.Request;
+import Utils.Config;
+import Utils.Response;
+import org.omg.CORBA.ORB;
+
+import java.net.SocketException;
+
+public class FrontEndImpl extends FEPOA {
+    private ORB orb;
+    private FIFO fifo = new FIFO();
+    private int leaderPort;
+
+    public FrontEndImpl() {
+        super();
+    }
+
+    public void setORB(ORB orb_val) {
+        this.orb = orb_val;
+    }
+
+    @Override
+    public String createTRecord(String managerID, String firstName, String lastName, String address, String phone, String specialization, String location) {
+        Request request = new Request(fifo.generateRequestNumber(managerID), managerID, Config.REQUEST.METHODS_NAME.createTRecord, firstName, lastName, address, phone, specialization, location);
+        return sendAndGetResponse(request);
+    }
+
+    @Override
+    public String createSRecord(String managerID, String firstName, String lastName, String coursesRegistered, String status) {
+        Request request = new Request(fifo.generateRequestNumber(managerID), managerID, Config.REQUEST.METHODS_NAME.createSRecord, firstName, lastName, coursesRegistered, status);
+        return sendAndGetResponse(request);
+    }
+
+    @Override
+    public String getRecordCounts(String managerID) {
+        Request request = new Request(fifo.generateRequestNumber(managerID), managerID, Config.REQUEST.METHODS_NAME.getRecordsCount);
+        return sendAndGetResponse(request);
+    }
+
+    @Override
+    public String editRecord(String managerID, String recordID, String fieldName, String newValue) {
+        Request request = new Request(fifo.generateRequestNumber(managerID), managerID, Config.REQUEST.METHODS_NAME.editRecord, recordID, fieldName, newValue);
+        return sendAndGetResponse(request);
+    }
+
+    @Override
+    public String transferRecord(String managerID, String recordID, String remoteCenterServerName) {
+        Request request = new Request(fifo.generateRequestNumber(managerID), managerID, Config.REQUEST.METHODS_NAME.transferRecord, recordID, remoteCenterServerName);
+        return sendAndGetResponse(request);
+    }
+
+    @Override
+    public String printRecord(String managerID, String recordID) {
+        Request request = new Request(fifo.generateRequestNumber(managerID), managerID, Config.REQUEST.METHODS_NAME.printRecord);
+        return sendAndGetResponse(request);
+    }
+
+    @Override
+    public String printAllRecords(String managerID) {
+        Request request = new Request(fifo.generateRequestNumber(managerID), managerID, Config.REQUEST.METHODS_NAME.printAllRecords);
+        return sendAndGetResponse(request);
+    }
+
+    private String sendAndGetResponse(Request request) {
+        try {
+            Unicast unicast = new Unicast(leaderPort);
+            unicast.send(request);
+            Response response = unicast.receive();
+            return response.getContent();
+        } catch (SocketException e) {
+            e.printStackTrace(System.out);
+        }
+        return "";
+    }
 }
--- src/Utils/Request.java
+++ src/Utils/Request.java
@@ -1,4 +1,4 @@
-package Servers;
+package Utils;
 
 import Utils.Config;
 
