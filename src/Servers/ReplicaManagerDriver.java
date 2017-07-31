9d60abaa438f40bed1d671ca55486b778ffa6262
Quoc Minh VU <minhvq27@gmail.com>
2017-07-31 15:13:56 -0400

    Add ManagerClient

--- src/Servers/ReplicaManagerDriver.java
+++ src/Servers/ReplicaManagerDriver.java
@@ -4,13 +4,13 @@ import Utils.Config;
 
 public class ReplicaManagerDriver {
     public static void main(String args[]) {
-//        ReplicaManager kenroReplica = new ReplicaManager(Config.ARCHITECTURE.REPLICAS.KEN_RO);
-//        Thread t1 = new Thread(kenroReplica);
-//        t1.start();
-//
-//        ReplicaManager kamalReplica = new ReplicaManager(Config.ARCHITECTURE.REPLICAS.KAMAL);
-//        Thread t2 = new Thread(kamalReplica);
-//        t2.start();
+        ReplicaManager kenroReplica = new ReplicaManager(Config.ARCHITECTURE.REPLICAS.KEN_RO);
+        Thread t1 = new Thread(kenroReplica);
+        t1.start();
+
+        ReplicaManager kamalReplica = new ReplicaManager(Config.ARCHITECTURE.REPLICAS.KAMAL);
+        Thread t2 = new Thread(kamalReplica);
+        t2.start();
 
         ReplicaManager minhReplica = new ReplicaManager(Config.ARCHITECTURE.REPLICAS.MINH);
         Thread t3 = new Thread(minhReplica);
